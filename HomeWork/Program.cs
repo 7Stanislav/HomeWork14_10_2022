4c583ec4f5b61fe44484b839782845ed725156a7
Stanislav <9323354@gmail.com>
2022-10-14 21:54:54 +0300

    Completed task 2

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -2,6 +2,7 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+/*
 Console.Write("Введите число N:");
 int n = int.Parse(Console.ReadLine());
 
@@ -14,3 +15,25 @@ string Recurs(int maxValue, int n)
     return (maxValue + " " + Recurs(maxValue - 1, n));
 }
 Console.WriteLine(Recurs(n, 1));
+*/
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M:");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число N:");
+int n = int.Parse(Console.ReadLine());
+
+int Recurs(int m, int n)
+{
+    if(m == n)
+    {
+        return n;
+    }
+    return (m + Recurs(m + 1, n));
+}
+Console.WriteLine(Recurs(m, n));
